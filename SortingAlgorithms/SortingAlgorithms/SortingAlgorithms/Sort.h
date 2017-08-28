449b6bd6245563b26fab177311385a47d0bc6570
hca <hca@itam.mx>
2017-08-28 12:30:12 -0500

    added sorting algorithms

---
+++ SortingAlgorithms/SortingAlgorithms/SortingAlgorithms/Sort.h
@@ -0,0 +1,61 @@
+#ifndef SORT_H
+#define SORT_H
+
+template <class T>
+class Sort {
+
+public:
+	Sort();
+	void static insertionSort(T * begin, T * end);
+	void static selectionSort(T * begin, T * end);
+
+private:
+
+
+};
+
+template <class T>
+Sort<T>::Sort() {
+
+}
+
+template <class T>
+void Sort<T>::insertionSort(T * begin, T * end) {
+	T * A = begin;
+	int n = end - begin;
+	int i, j;
+	T aux;
+
+	for (i = 1; i < n; i++) {
+		j = i - 1;
+		aux = A[i];
+
+		while (j >= 0 && aux < A[j]) {
+			A[j + 1] = A[j];
+			j--;
+		}
+
+		A[j + 1] = aux;
+	}
+}
+
+template <class T>
+void Sort<T>::selectionSort(T * begin, T * end) {
+	T * A = begin;
+	int n = end - begin;
+	int i, j, minIdx;
+
+	for (i = 0; i < n; i++) {
+		minIdx = i;
+		
+		for (j = minIdx + 1; j < n; j++)
+			if (A[j] < A[minIdx])
+				minIdx = j;
+
+		std::swap(A[i], A[minIdx]);
+	}
+
+}
+
+#endif // !SORT_H
+
---
+++ SortingAlgorithms/SortingAlgorithms/SortingAlgorithms/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Sort.h"
+
+int main() {
+	int A[10] = { 5, 5, 9, 8, 0, 3, 12, 1, 7, 10 };
+	Sort<int>::selectionSort(A, A + 10);
+
+	int i;
+	for (i = 0; i < 10; i++)
+		std::cout << A[i] << " ";
+	std::cout << "\n";
+
+	int a;
+	std::cin >> a;
+
+	return 0;
+}
