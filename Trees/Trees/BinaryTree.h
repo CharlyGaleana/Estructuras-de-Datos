9c63025d5f3ecf85a54e416bb19c8afc85a9cffd
hca <hca@itam.mx>
2017-09-11 12:39:11 -0500

    Added Binary Search Tree functions

--- Trees/Trees/BinaryTree.h
+++ Trees/Trees/BinaryTree.h
@@ -1,14 +1,43 @@
+#include <vector>
+
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
 
 template <class T>
 class BinaryTree {
 public:
-	virtual bool isEmpty();
-	virtual int size();
-	virtual bool contains(T data);
-	virtual void insert();
-	virtual BinaryTree<T>::iterator inOrder();
+	virtual bool isEmpty() = 0;
+	virtual int size() = 0;
+	virtual bool contains(T data) = 0;
+	virtual void insert(T data) = 0;
+	virtual std::vector<T> preOrder() = 0;
+	virtual std::vector<T> inOrder() = 0;
 };
+ 
+//template <class T>
+//bool BinaryTree<T>::isEmpty() {
+//
+//}
+//
+//template <class T>
+//int BinaryTree<T>::size() {
+//	return 0;
+//}
+//
+//template <class T>
+//bool BinaryTree<T>::contains(T data) {
+//	return true;
+//}
+//
+//template <class T>
+//void BinaryTree<T>::insert(T data) {
+//
+//}
+//
+//template <class T>
+//std::vector<T> BinaryTree<T>::preOrder() {
+//	
+//}
+
 
 #endif
--- Trees/Trees/main.cpp
+++ Trees/Trees/main.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include "LinkedBinaryTree.h"
+
 int main() {
+
+	LinkedBinaryTree<int> * tree = new LinkedBinaryTree<int>;
+	tree->insert(1);
+	tree->insert(2);
+	tree->insert(4);
+	tree->insert(3);
+	tree->insert(8);
+	tree->insert(9);
+	tree->insert(1);
+	tree->insert(0);
+	tree->insert(-1);
+
+	std::vector<int> p = tree->inOrder();
+	for (int i = 0; i < p.size(); i++)
+		std::cout << p[i] << " ";
+	std::cout << "\n";
+
+	std::cout << tree->depth();
+
+	int a;
+	std::cin >> a;
+
 	return 0;
 }
